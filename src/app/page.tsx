c7693cdadacdd1fdd30ee6bb355486292d1e5b34
youngmatias <seyoit@gmail.com>
2024-05-29 15:11:59 +0900

    feat:Add Jotai/axios, Add login/nick-name pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@
 import { MyButton } from '@/components/MyButton';
 import { Button } from '@/stories/Button';
 import Image from 'next/image';
+import { atom } from 'jotai';
 
+export const nickNameAtom = atom('nick');
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
