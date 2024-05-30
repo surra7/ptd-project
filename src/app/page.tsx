fc6bc0ba5ee00c75efb64140740f49049f87b1b7
youngmatias <seyoit@gmail.com>
2024-05-30 11:55:19 +0900

    fix:add login/nick name buttons for test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 'use client';
-import { MyButton } from '@/components/MyButton';
+import { Button } from '@/stories/Button';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>메인 페이지</div>
-      <MyButton children="버튼1" />
+      <Link href="/login">
+        <Button label="login" />
+      </Link>
+      <Link href="/login">
+        <Button label="nick name" />
+      </Link>
     </main>
   );
 }
