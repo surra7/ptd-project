abd5a9a7087ff5248f2642dd65ba678466545b3e
youngmatias <seyoit@gmail.com>
2024-05-30 12:09:49 +0900

    fix:for team review

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 'use client';
 import { Button } from '@/stories/Button';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>메인 페이지</div>
-      <Link href="/login">
-        <Button label="login" />
-      </Link>
-      <Link href="/nick-name">
-        <Button label="nick name" />
-      </Link>
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <div>
+        <Image src="/images/logoPurple.png" alt="Logo" width={100} height={100} />
+      </div>
+      <div className="flex  mt-8">
+        <Link className="mr-4" href="/login">
+          <Button label="Login" />
+        </Link>
+        <Link href="/nick-name">
+          <Button label="Nick Name" />
+        </Link>
+      </div>
     </main>
   );
 }
