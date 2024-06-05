c5e885a52917c3f2e975c6bee4255c057c759488
togom <jkisung1@naver.com>
2024-06-05 20:26:22 +0900

    Refactor: TODO list 코드 리팩토링, Style 추가

--- src/app/todolist/page.tsx
+++ src/app/todolist/page.tsx
@@ -1,23 +1,25 @@
 'use client';
+import NavBottom from '@/components/NavBottom';
 import Modal from '@/components/todo/Modal';
+import Mood from '@/components/todo/Mood';
 import Timer from '@/components/todo/Timer';
 import ToDoInsert from '@/components/todo/ToDoInsert';
-import ToDoListItem from '@/components/todo/todolistitem';
+import ToDoListItem from '@/components/todo/ToDoListItem';
 import React, { useCallback, useRef, useState } from 'react';
 import { IoMusicalNotesOutline } from 'react-icons/io5';
-import { TbMoodEmpty, TbMoodSad, TbMoodSmile } from 'react-icons/tb';
 
-export type TodoType = {
+export interface TodoType {
   id: number;
   text: string;
   checked: boolean;
-};
+}
 
 function Page() {
   const [value, setValue] = useState<string>('');
   const [todos, setTodos] = useState<TodoType[]>([]);
   const [isModalOpen, setIsModalOpen] = useState<boolean>();
   const [modalIndex, setModalIndex] = useState<number>();
+  const [todoIndex, setTodoIndex] = useState<number>();
 
   const nextId = useRef(0);
   const onInsert = useCallback(
@@ -30,13 +32,13 @@ function Page() {
       if (!text) return alert('TODO를 적어주세요!');
       setTodos(todos.concat(todo));
       nextId.current++;
-      console.log(todo.id);
+      console.log(todos);
     },
     [todos],
   );
 
   const openModal = useCallback((index: number) => {
-    setIsModalOpen(true);
+    setIsModalOpen(isModalOpen => !isModalOpen);
     console.log(index);
     if (modalIndex != undefined) {
       setModalIndex(undefined);
@@ -55,61 +57,66 @@ function Page() {
     [todos],
   );
 
+  const checkIndex = useCallback((id: number) => {
+    setTodoIndex(id);
+  }, []);
+
   return (
-    <div className="flex justify-center bg-black">
-      <div className="w-[23.4375rem] h-[50.75rem] bg-white">
-        <main className="w-full h-auto top-[3.4375rem] relative ">
-          <header className="flex flex-col items-center">
-            <div className="flex w-full h-[3.1875rem] px-5 justify-between items-center border-b-[0.0313rem] border-borderGray">
-              <p className="font-bold text-textGray text-[1.125rem]">이번 달 프로젝트 잘 끝내기</p>
-              <div className="w-[3.75rem] h-[2.375rem] text-veryPurple font-extrabold text-[1.625rem] text-center flex justify-center items-center">
-                D-32
-              </div>
+    <div className="flex container justify-center w-full h-[50.75rem] bg-white">
+      <main className="w-full h-auto top-[3.4375rem] relative ">
+        <header className="flex flex-col items-center">
+          <div className="flex w-full h-[3.1875rem] px-5 justify-between items-center border-b-[0.0313rem] border-borderGray">
+            <p className="font-bold text-black-900 text-[1.125rem]">이번 달 프로젝트 잘 끝내기</p>
+            <div className="w-[3.75rem] h-[2.375rem] text-primary-600 font-extrabold text-[1.625rem] text-center flex justify-center items-center">
+              D-32
             </div>
-            <section className="w-full h-[2.5625rem] flex border-b-[0.0313rem] border-borderGray items-center">
-              <div className="flex w-[8.8125rem] h-[2.5625rem] border-r-[0.0313rem] border-borderGray relative items-center justify-center text-[0.8125rem] font-medium text-textGray">
-                2024. 05. 28
-              </div>
-              <div className="w-[15.5rem] h-[2.5625rem] px-[0.625rem] flex justify-between items-center">
-                <div className="flex w-[4.25rem] h-[1.5625rem] gap-[0.1875rem] items-center">
-                  <TbMoodSmile className="w-[1.5625rem] h-[1.5625rem] text-veryPurple" />
-                  <p className="text-[0.75rem] font-medium text-veryPurple">HAPPY</p>
-                </div>
-                <div className="flex w-[4.25rem] h-[1.5625rem] gap-[0.1875rem] items-center">
-                  <TbMoodEmpty className="w-[1.5625rem] h-[1.5625rem] text-borderGray" />
-                  <p className="text-[0.75rem] font-medium text-borderGray">SOSO</p>
-                </div>
-                <div className="flex w-[4.25rem] h-[1.5625rem] gap-[0.1875rem] items-center">
-                  <TbMoodSad className="w-[1.5625rem] h-[1.5625rem] text-borderGray" />
-                  <p className="text-[0.75rem] font-medium text-borderGray">SAD</p>
-                </div>
-              </div>
-            </section>
-            <section>
-              <div className="flex items-center justify-around w-[23.4375rem] h-[2.5625rem] border-b-[0.0313rem] border-borderGray px-[0.625rem] gap-[0.3125rem]">
-                <Timer />
-              </div>
-            </section>
-            <section className="flex w-full h-[2.5625rem] items-center px-[0.625rem] gap-[0.625rem] border-b-[0.0313rem] border-borderGray">
-              <div>
-                <IoMusicalNotesOutline className="w-[1.25rem] h-[1.25rem]" />
-              </div>
-              <div className="text-[0.75rem] font-medium text-textGray">심규선 - care</div>
-            </section>
-          </header>
-          <section className="flex flex-col w-full h-[20rem] overflow-auto">
-            <div className="border-r-[0.0313rem] border-borderGray w-[2.75rem] h-[20rem] absolute" />
-            <ToDoListItem todos={todos} openModal={openModal} />
+          </div>
+          <section className="w-full h-[2.5625rem] flex border-b-[0.0313rem] border-borderGray items-center">
+            <div className="flex w-[8.8125rem] h-[2rem] border-r-[0.0313rem] border-borderGray relative items-center justify-center text-[0.8125rem] font-medium text-black-900">
+              2024. 05. 28
+            </div>
+            <div className="w-[15.5rem] h-[2rem] px-[0.625rem] flex justify-between items-center">
+              <Mood />
+            </div>
           </section>
-          <ToDoInsert onInsert={onInsert} value={value} setValue={setValue} />
-          {isModalOpen ? <Modal onRemove={onRemove} id={modalIndex as number} /> : <></>}
           <section>
-            <p className="m-2 pb-1 border-b-borderGray border-b w-[3.0625rem] text-center">Memo</p>
-            <textarea name="memo" id="memo" className="w-full h-[5rem] text-[0.875rem] px-2 resize-none"></textarea>
+            <div className="flex items-center justify-around w-[23.4375rem] h-[2.5625rem] border-b-[0.0313rem] border-borderGray px-[0.625rem] gap-[0.3125rem]">
+              <Timer />
+            </div>
           </section>
-          <footer className="w-full h-[5.25rem]">하단 글로벌 네비게이터 들어갈 자리 입니다</footer>
-        </main>
-      </div>
+          <section className="flex w-full h-[2.5625rem] items-center px-[0.625rem] gap-[0.625rem] border-b-[0.0313rem] border-borderGray">
+            <div>
+              <IoMusicalNotesOutline className="w-[1.25rem] h-[1.25rem]" />
+            </div>
+            <div className="text-[0.75rem] font-medium text-black-900">심규선 - care</div>
+          </section>
+        </header>
+        <section className="flex flex-col w-full h-[22rem] overflow-auto">
+          <div className="border-r-[0.0313rem] border-borderGray w-[2.75rem] h-[22rem] absolute" />
+          {todos.map(todo => (
+            <ToDoListItem
+              todo={todo}
+              openModal={openModal}
+              isModalOpen={isModalOpen as boolean}
+              todoIndex={todoIndex as number}
+              checkIndex={checkIndex}
+              modalIndex={modalIndex as number}
+            />
+          ))}
+        </section>
+        <ToDoInsert onInsert={onInsert} value={value} setValue={setValue} />
+        {isModalOpen ? <Modal onRemove={onRemove} id={modalIndex as number} /> : <></>}
+        <section>
+          <p className="m-2 pb-1 border-b-borderGray border-b w-[3.0625rem] text-center">Memo</p>
+          <textarea
+            name="memo"
+            id="memo"
+            className="w-full h-[6rem] text-[0.875rem] px-2 resize-none focus:outline-none"></textarea>
+        </section>
+        <footer className="w-full h-[5.25rem]">
+          <NavBottom />
+        </footer>
+      </main>
     </div>
   );
 }
--- src/components/todo/ToDoListItem.tsx
+++ src/components/todo/ToDoListItem.tsx
@@ -1,28 +1,49 @@
 import { TodoType } from '@/app/todolist/page';
+import { useState } from 'react';
 import { BsThreeDots } from 'react-icons/bs';
+import { IoMdCheckbox } from 'react-icons/io';
+import { MdCheckBoxOutlineBlank } from 'react-icons/md';
 
 interface Props {
-  todos: TodoType[];
+  todo: TodoType;
   openModal(index: number): void;
+  isModalOpen: boolean;
+  todoIndex: number;
+  checkIndex(id: number): void;
+  modalIndex: number;
 }
 
-function ToDoListItem({ todos, openModal }: Props) {
+function ToDoListItem({ todo, openModal, isModalOpen, todoIndex, checkIndex, modalIndex }: Props) {
+  const [isCompleted, setIsCompleted] = useState<boolean>(false);
+
+  const handleComplete = (id: number) => {
+    if (id === todoIndex) {
+      setIsCompleted(isCompleted => !isCompleted);
+      console.log(id);
+      console.log(todoIndex);
+    }
+  };
+
   return (
     <>
-      {todos.map(todo => (
-        <div className="flex items-center" key={todo.id}>
-          <label className="flex w-full h-[2.625rem] items-center py-[0.765rem] pl-[0.765rem]">
-            <input type="checkbox" name="잠자기" value={1} className="w-[1.25rem] h-[1.25rem] z-10" />
-            <span className="flex w-full h-auto pl-[1.5rem] items-center justify-between">
-              <p className="text-Gray  font-medium">{todo.text}</p>
-            </span>
-          </label>
-          <BsThreeDots
-            className="w-[50px] text-borderGray text-[1.25rem] px-[0.625rem] cursor-pointer"
-            onClick={() => openModal(todo.id)}
-          />
-        </div>
-      ))}
+      <div className="flex items-center" key={todo.id} onClick={() => checkIndex(todo.id)}>
+        <label
+          className="flex w-full h-[2.625rem] items-center py-[0.765rem] pl-[0.765rem]"
+          onClick={() => handleComplete(todo.id)}>
+          {isCompleted ? (
+            <IoMdCheckbox className="text-primary-400 w-[1.25rem] h-[1.25rem] z-[1]" />
+          ) : (
+            <MdCheckBoxOutlineBlank className="w-[1.25rem] h-[1.25rem] z-[1]" />
+          )}
+          <span className="flex w-full h-auto pl-[1.5rem] items-center justify-between">
+            <p className={`${isCompleted ? 'text-black-200' : 'text-black-900'} font-medium`}>{todo.text}</p>
+          </span>
+        </label>
+        <BsThreeDots
+          className={`w-[50px] text-primary- ${modalIndex === todo.id && isModalOpen ? 'text-primary-400' : 'text-black-200'} text-[1.25rem] px-[0.625rem] cursor-pointer`}
+          onClick={() => openModal(todo.id)}
+        />
+      </div>
     </>
   );
 }
