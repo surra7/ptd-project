1ffe29d6276f670495fe33051480d7a46fdd6546
togom <jkisung1@naver.com>
2024-06-12 16:11:34 +0900

    Feat: Modal 스타일 변경 / 바깥 클릭시 모달창 닫힘 기능 추가

--- src/components/todo/Modal.tsx
+++ src/components/todo/Modal.tsx
@@ -1,16 +1,27 @@
+import { Dispatch } from 'react';
+
 interface Props {
   onRemove(id?: number): void;
   onEdit(id?: number): void;
   id?: number;
+  setIsModalOpen(boolean: boolean): void;
 }
-function Modal({ onRemove, onEdit, id }: Props) {
+function Modal({ onRemove, onEdit, id, setIsModalOpen }: Props) {
   return (
-    <div className="w-full flex flex-col justify-center h-[8.5rem] pl-3 gap-7 bg-[#F2F2F2] absolute bottom-[4rem] rounded-t-[0.625rem]">
-      <button className="flex font-semibold text-[1.125rem] w-full" onClick={() => onEdit(id)}>수정</button>
-      <button className="flex font-semibold text-[1.125rem] w-full text-errorRed" onClick={() => onRemove(id)}>
-        삭제
-      </button>
-    </div>
+    <>
+      <div
+        className="absolute top-0 left-0 w-full h-full bg-black-950 bg-opacity-50 z-10"
+        onClick={() => setIsModalOpen(false)}
+      />
+      <div className="w-full flex flex-col justify-center h-[8.5rem] pl-3 gap-7 bg-[#F2F2F2] absolute bottom-0 rounded-t-[0.625rem] z-20">
+        <button className="flex font-semibold text-[1.125rem] w-full" onClick={() => onEdit(id)}>
+          수정
+        </button>
+        <button className="flex font-semibold text-[1.125rem] w-full text-errorRed" onClick={() => onRemove(id)}>
+          삭제
+        </button>
+      </div>
+    </>
   );
 }
 
