d37b67c8da39ac14d5bcb6cb274ea34675043be2
togom <jkisung1@naver.com>
2024-06-12 16:11:05 +0900

    Feat: TODO list API 연동

--- src/app/todolist/page.tsx
+++ src/app/todolist/page.tsx
@@ -2,97 +2,139 @@
 import NavBottom from '@/components/NavBottom';
 import Modal from '@/components/todo/Modal';
 import Mood from '@/components/todo/Mood';
+import MusicInput from '@/components/todo/MusicInput';
 import Timer from '@/components/todo/Timer';
 import ToDoInsert from '@/components/todo/ToDoInsert';
 import ToDoListItem from '@/components/todo/ToDoListItem';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import React, { MutableRefObject, useCallback, useEffect, useRef, useState } from 'react';
 import { IoMusicalNotesOutline } from 'react-icons/io5';
 
 export interface TodoType {
-  id: number;
-  text: string;
-  checked: boolean;
+  todo_item: string;
+  id: number | undefined;
+  done: boolean;
 }
 
 function Page() {
   const [todos, setTodos] = useState<TodoType[]>([]);
-  const [isModalOpen, setIsModalOpen] = useState<boolean>();
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [modalIndex, setModalIndex] = useState<number>();
   const [editString, setEditString] = useState<string>();
-  const id = useRef(1);
+  const [musicTitle, setMusicTitle] = useState<string | undefined>();
+  const [musicUrl, setMusicUrl] = useState<string>('');
 
+  const id = useRef(1);
   const onInsert = useCallback(
-    (text?: string) => {
-      console.log(todos);
+    async (text?: string) => {
       if (text) {
         if (editString !== undefined && modalIndex !== undefined) {
-          const edit = todos.find((v) => v.id === modalIndex);
+          const edit = todos.find(v => v.id === modalIndex);
           if (edit !== undefined) {
-            edit.text = text;
+            edit.todo_item = text;
             setTodos([...todos]);
           }
-          
+          await axios.put(`https://api.oz-02-main-04.xyz/api/v1/posts/todo/1/${modalIndex}`, {
+            todo_item: text,
+          });
           setEditString(undefined);
           setModalIndex(undefined);
         } else {
           const todo = {
+            todo_item: text,
             id: id.current,
-            text,
-            checked: false,
+            done: false,
           };
-          setTodos((prev) => prev.concat(todo));
+          await axios.post('https://api.oz-02-main-04.xyz/api/v1/posts/todo/1', todo);
+          const res = await axios.get('https://api.oz-02-main-04.xyz/api/v1/posts/todo/1');
+          setTodos(res.data);
           ++id.current;
         }
       } else {
-        alert('TODO를 적어주세요!')
+        alert('TODO를 적어주세요!');
       }
     },
     [todos, editString, modalIndex],
   );
 
-  const openModal = useCallback((index: number) => {
-    setModalIndex(index);
-    setEditString(undefined);
-    if (index === modalIndex) {
-      setIsModalOpen((prev) => !prev);
-    } else {
-      setIsModalOpen(true);
-    }
-  }, [modalIndex]);
+  const openModal = useCallback(
+    (index: number) => {
+      setModalIndex(index);
+      setEditString(undefined);
+      if (index === modalIndex) {
+        setIsModalOpen(prev => !prev);
+      } else {
+        setIsModalOpen(true);
+      }
+    },
+    [modalIndex],
+  );
 
   const onRemove = useCallback(
-    (id?: number) => {
+    async (id?: number) => {
       if (id != undefined) {
         setTodos(todos.filter(todo => todo.id !== id));
       }
+      await axios.delete(`https://api.oz-02-main-04.xyz/api/v1/posts/todo/1/${id}`);
       setIsModalOpen(false);
       setModalIndex(undefined);
     },
     [todos],
   );
 
-  const onEdit = useCallback((id?: number) => {
-    if (id != undefined) {
-      const edit = todos.find((v) => v.id === id);
-      setEditString(edit?.text);
-      setIsModalOpen(false);
-    }
-  }, [todos]);
+  const onEdit = useCallback(
+    (id?: number) => {
+      if (id != undefined) {
+        const edit = todos.find(v => v.id === id);
+        setEditString(edit?.todo_item);
+        setIsModalOpen(false);
+      }
+    },
+    [todos],
+  );
 
-  const handleComplete = useCallback((id: number) => {
-    const checked = todos.find((v) => v.id === id);
-    if (checked !== undefined) {
-      checked.checked = !checked.checked;
-      setTodos([...todos]);
-    }
-  }, [todos]);
+  const handleComplete = useCallback(
+    async (id: number) => {
+      const checked = todos.find(v => v.id === id);
+      if (checked !== undefined) {
+        checked.done = !checked.done;
+        await axios.put(`https://api.oz-02-main-04.xyz/api/v1/posts/todo/1/${id}`, {
+          done: checked.done,
+        });
+        setTodos([...todos]);
+      }
+    },
+    [todos],
+  );
 
-  useEffect(() => {console.log(todos)}, [todos]);
+  useEffect(() => {
+    const fetchTodo = async () => {
+      try {
+        const response = await axios.get('https://api.oz-02-main-04.xyz/api/v1/posts/todo/1');
+        setTodos(response.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchTodo();
+  }, []);
+
+  // useEffect(() => {
+  //   const fetchMusic = async () => {
+  //     try {
+  //       const response = await axios.get('https://api.oz-02-main-04.xyz/api/v1/posts/music/1');
+  //       console.log(response.data);
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //   };
+  //   fetchMusic();
+  // }, []);
 
   return (
-    <div className="flex container justify-center w-full h-[50.75rem] bg-white">
-      <main className="w-full h-auto top-[3.4375rem] relative ">
-        <header className="flex flex-col items-center">
+    <div className="w-full h-full">
+      <main className="wrap-section relative ">
+        <header className="pt-[2rem]">
           <div className="flex w-full h-[3.1875rem] px-5 justify-between items-center border-b-[0.0313rem] border-borderGray">
             <p className="font-bold text-black-900 text-[1.125rem]">이번 달 프로젝트 잘 끝내기</p>
             <div className="w-[3.75rem] h-[2.375rem] text-primary-600 font-extrabold text-[1.625rem] text-center flex justify-center items-center">
@@ -108,7 +150,7 @@ function Page() {
             </div>
           </section>
           <section>
-            <div className="flex items-center justify-around w-[23.4375rem] h-[2.5625rem] border-b-[0.0313rem] border-borderGray px-[0.625rem] gap-[0.3125rem]">
+            <div className="flex items-center justify-around w-full h-[2.5625rem] border-b-[0.0313rem] border-borderGray px-[0.625rem] gap-[0.3125rem]">
               <Timer />
             </div>
           </section>
@@ -116,13 +158,23 @@ function Page() {
             <div>
               <IoMusicalNotesOutline className="w-[1.25rem] h-[1.25rem]" />
             </div>
-            <div className="text-[0.75rem] font-medium text-black-900">심규선 - care</div>
+            <div className="w-full text-[0.75rem] font-medium text-black-900">
+              {musicTitle ? (
+                <div className="w-full flex items-center justify-between">
+                  <span onClick={() => setMusicTitle('')}>{musicTitle}</span>
+                  <audio controls src={musicUrl} className="w-[11rem] h-[2.5rem]"></audio>
+                </div>
+              ) : (
+                <MusicInput setMusicTitle={setMusicTitle} setMusicUrl={setMusicUrl} />
+              )}
+            </div>
           </section>
         </header>
         <section className="flex flex-col w-full h-[22rem] overflow-auto">
           <div className="border-r-[0.0313rem] border-borderGray w-[2.75rem] h-[22rem] absolute" />
           {todos.map((todo, key) => (
             <ToDoListItem
+              id={todo.id}
               key={key}
               todo={todo}
               openModal={openModal}
@@ -133,7 +185,11 @@ function Page() {
           ))}
         </section>
         <ToDoInsert onInsert={onInsert} init={editString} />
-        {isModalOpen ? <Modal onRemove={onRemove} onEdit={onEdit} id={modalIndex} /> : <></>}
+        {isModalOpen ? (
+          <Modal onRemove={onRemove} onEdit={onEdit} id={modalIndex} setIsModalOpen={setIsModalOpen} />
+        ) : (
+          <></>
+        )}
         <section>
           <p className="m-2 pb-1 border-b-borderGray border-b w-[3.0625rem] text-center">Memo</p>
           <textarea
@@ -141,10 +197,8 @@ function Page() {
             id="memo"
             className="w-full h-[6rem] text-[0.875rem] px-2 resize-none focus:outline-none"></textarea>
         </section>
-        <footer className="w-full h-[5.25rem]">
-          <NavBottom />
-        </footer>
       </main>
+      <NavBottom />
     </div>
   );
 }
--- src/components/todo/ToDoListItem.tsx
+++ src/components/todo/ToDoListItem.tsx
@@ -1,31 +1,32 @@
 import { TodoType } from '@/app/todolist/page';
-import { useState } from 'react';
+import { MutableRefObject, useState } from 'react';
 import { BsThreeDots } from 'react-icons/bs';
 import { IoMdCheckbox } from 'react-icons/io';
 import { MdCheckBoxOutlineBlank } from 'react-icons/md';
 
 interface Props {
   todo: TodoType;
-  openModal(index: number): void;
-  handleComplete(index: number): void;
+  openModal(index: number | undefined): void;
+  handleComplete(index: number | undefined): void;
   isModalOpen?: boolean;
   modalIndex?: number;
+  id: number | undefined;
 }
 
-function ToDoListItem({ todo, openModal, handleComplete, isModalOpen, modalIndex }: Props) {
+function ToDoListItem({ todo, openModal, handleComplete, isModalOpen, modalIndex, id }: Props) {
   return (
     <>
-      <div className="flex items-center" >
+      <div className="flex items-center">
         <label
           className="flex w-full h-[2.625rem] items-center py-[0.765rem] pl-[0.765rem]"
           onClick={() => handleComplete(todo.id)}>
-          {todo.checked ? (
+          {todo.done ? (
             <IoMdCheckbox className="text-primary-400 w-[1.25rem] h-[1.25rem] z-[1]" />
           ) : (
             <MdCheckBoxOutlineBlank className="w-[1.25rem] h-[1.25rem] z-[1]" />
           )}
           <span className="flex w-full h-auto pl-[1.5rem] items-center justify-between">
-            <p className={`${todo.checked ? 'text-black-200' : 'text-black-900'} font-medium`}>{todo.text}</p>
+            <p className={`${todo.done ? 'text-black-200' : 'text-black-900'} font-medium`}>{todo.todo_item}</p>
           </span>
         </label>
         <BsThreeDots
