1436b97905e46de5c3563ee49d8282579871be9b
youngmatias <seyoit@gmail.com>
2024-06-14 11:23:08 +0100

    url change

--- src/app/goal/page.tsx
+++ src/app/goal/page.tsx
@@ -16,7 +16,7 @@ function Goal() {
     if (!user) return;
     try {
       const response = await axios.post(
-        `https://api.oz-02-main-04.xyz/api/v1/users/${user.id}/goal/`,
+        `https://api.oz-02-main-04.xyz/api/v1/users/goal/${user.id}`,
         { goal: goal, d_day: dDay },
         {
           withXSRFToken: true,
