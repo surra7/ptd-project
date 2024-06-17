97ce37e89c4ba313f42ee3b32de6f8fcc27444c3
youngmatias <seyoit@gmail.com>
2024-06-17 06:31:40 +0200

    login redirect

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,20 @@
 'use client';
 import Image from 'next/image';
+import { userAtom } from '@/atoms/atoms';
+import { useAtom } from 'jotai';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
 const KakaoLogin = () => {
+  const router = useRouter();
+  const [user] = useAtom(userAtom);
+
+  useEffect(() => {
+    if (user) {
+      router.push('/');
+    }
+  }, [user, router]);
+
   const handleKakaoLogin = () => {
     const kakaoAuthUrl = `https://api.oz-02-main-04.xyz/api/v1/users/kakao/`;
     window.location.href = kakaoAuthUrl;
@@ -8,14 +22,8 @@ const KakaoLogin = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      {' '}
-      <button>
-        <Image
-          onClick={handleKakaoLogin}
-          src={'/images/kakaoLogin.png'}
-          alt="kakao-login"
-          width={200}
-          height={200}></Image>
+      <button onClick={handleKakaoLogin}>
+        <Image src={'/images/kakaoLogin.png'} alt="kakao-login" width={200} height={200} />
       </button>
     </div>
   );
