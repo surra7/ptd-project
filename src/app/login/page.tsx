ec63be47b6b3ed62713f9ba7cdd321992c9cc6ef
youngmatias <seyoit@gmail.com>
2024-06-18 07:08:47 +0200

    login fix2

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -19,7 +19,7 @@ const KakaoLogin = () => {
   //   }, [user, router]);
 
   const handleKakaoLogin = () => {
-    setNickname('nickname changed');
+    // setNickname('nickname changed');
     const kakaoAuthUrl = `https://api.oz-02-main-04.xyz/api/v1/users/kakao/`;
     window.location.href = kakaoAuthUrl;
   };
@@ -28,7 +28,7 @@ const KakaoLogin = () => {
     <div className="flex flex-col items-center justify-center min-h-screen  p-4">
       <button onClick={handleKakaoLogin}>
         {' '}
-        {nickname}
+        {/* {nickname} */}
         <Image src={'/images/kakaoLogin.png'} alt="kakao-login" width={200} height={200} />
       </button>
     </div>
