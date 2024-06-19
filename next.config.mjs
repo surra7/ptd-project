ebeb9f208589d24809fdd84ab663cb61c939faef
eoekstpal <67219663+eoekstpal@users.noreply.github.com>
2024-06-19 21:20:13 +0900

    Mainpage (#97)
    * Feat: 수정 main axios 이미지주소
    
    * Feat: mainpage userdata
    
    * 수정 api 주소
    
    * main axios 수정
    
    * api 이미지 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {reactStrictMode: false,};
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'api.oz-02-main-04.xyz' }],
+  },
+};
 
 export default nextConfig;
