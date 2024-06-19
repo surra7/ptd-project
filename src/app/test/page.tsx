e30d2c8c768425306923cabc456df27507b5ff9c
youngmatias <seyoit@gmail.com>
2024-06-19 08:30:13 +0200

    fix

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { User, userAtom } from '@/atoms/atoms';
 import { useAtom } from 'jotai';
+
 function Test2() {
   const [user, setUser] = useAtom<User | null>(userAtom);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      {user?.id} {user?.계정}
+      <p>User ID: {user.id}</p>
+      <p>Account: {user['계정']}</p>
     </div>
   );
 }
