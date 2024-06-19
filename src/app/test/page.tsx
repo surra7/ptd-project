5b4126fa0a5b7a2442308b7da01bdcd7fa471091
youngmatias <seyoit@gmail.com>
2024-06-19 07:43:11 +0200

    test2

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { User, userAtom } from '@/atoms/atoms';
 import { useAtom } from 'jotai';
-function page() {
+function Test() {
   const [user, setUser] = useAtom<User | null>(userAtom);
   return (
     <div>
@@ -10,4 +10,4 @@ function page() {
   );
 }
 
-export default page;
+export default Test;
