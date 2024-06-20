bbab8da3eac6a6387defdc791c9ccf7f36a39e29
togom <jkisung1@naver.com>
2024-06-20 10:59:59 +0900

    Feat: 전날 투두 기능 막기

--- src/app/monthly/[postId]/ToDoInsert2.tsx
+++ src/app/monthly/[postId]/ToDoInsert2.tsx
@@ -9,32 +9,15 @@ interface Props {
 function ToDoInsert({ init, onInsert }: Props) {
   const [value, setValue] = useState<string>(init ?? '');
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-  };
-
-  const onSubmit = useCallback(
-    (e: React.FormEvent) => {
-      setValue('');
-      e.preventDefault();
-      onInsert(value);
-    },
-    [value, onInsert],
-  );
-
   useEffect(() => {
     setValue(init ?? '');
   }, [init]);
 
   return (
-    <form className="w-full h-[2.625rem] flex" onSubmit={onSubmit}>
-      <input
-        type="text"
-        className="border-black-200 border w-full h-[2.625rem] pl-2 focus:outline-none"
-        placeholder="TODO는 이곳에 적어주세요!"
-        onChange={onChange}
-        value={value}
-      />
+    <form className="w-full h-[2.625rem] flex" onClick={e => e.preventDefault()}>
+      <div className="border-black-200 flex items-center text-black-400 bg-black-100 border w-full h-[2.625rem] pl-2 focus:outline-none">
+        <p>지난 할 일 목록은 쓸 수 없습니다.</p>
+      </div>
       <button
         className={`flex items-center justify-center w-[2.625rem] h-[2.625rem] ${value ? 'bg-primary-400' : 'bg-black-200'} active:bg-primary-200`}>
         <AiOutlinePlus className="text-[2rem] text-white" />
