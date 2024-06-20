548763226aef877422a4ea114ddeafb94fbe0b7b
youngmatias <seyoit@gmail.com>
2024-06-20 05:12:47 +0200

    fin

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -99,59 +99,58 @@ export default function Page() {
       {' '}
       {user ? (
         <>
-          <div className=" flex flex-col p-4 mt-10">
-            <section className="wrap-section">
-              {' '}
-              <p className="text-2xl font-bold text-purple-600 mb-4">안녕하세요! {user.닉네임} 님</p>
-              <hr className="border-purple-600 w-full mb-4" />
-              <ul className="space-y-4">
-                <li>
-                  <Link href="/nickname">
-                    <p className="text-purple-600 hover:underline">닉네임 변경하기</p>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/goal">
-                    <p className="text-purple-600 hover:underline">목표 설정하기</p>
-                  </Link>
-                </li>
-                <li>
-                  {' '}
-                  <Link href="/introduce">
-                    <p className="text-purple-600 hover:underline">petodo 가이드 보기</p>
-                  </Link>{' '}
-                </li>
-                <li>
-                  <a onClick={handleLogout} className="text-purple-600 hover:underline">
-                    로그아웃
-                  </a>
-                </li>
-              </ul>
-              <hr className="border-purple-600 w-full my-4" />
-            </section>
-            <NavBottom />{' '}
-          </div>
+          {/* <div className=" flex flex-col p-4 mt-10"> */}
+          <section className="wrap-section">
+            {' '}
+            <p className="text-2xl font-bold text-purple-600 mb-4">안녕하세요! {user.닉네임} 님</p>
+            <hr className="border-purple-600 w-full mb-4" />
+            <ul className="space-y-4">
+              <li>
+                <Link href="/nickname">
+                  <p className="text-purple-600 hover:underline">닉네임 변경하기</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/goal">
+                  <p className="text-purple-600 hover:underline">목표 설정하기</p>
+                </Link>
+              </li>
+              <li>
+                {' '}
+                <Link href="/introduce">
+                  <p className="text-purple-600 hover:underline">petodo 가이드 보기</p>
+                </Link>{' '}
+              </li>
+              <li>
+                <a onClick={handleLogout} className="text-purple-600 hover:underline">
+                  로그아웃
+                </a>
+              </li>
+            </ul>
+            <hr className="border-purple-600 w-full my-4" />
+          </section>
+          <NavBottom /> {/* </div> */}
         </>
       ) : (
         <>
-          <div className=" flex flex-col justify-center align-middle  p-4">
-            <section
-              className="wrap-section
+          {/* <div className=" flex flex-col justify-center align-middle  p-4"> */}
+          <section
+            className="wrap-section
             ">
-              <p className="text-2xl font-bold text-purple-600 mb-4">로그인 해주세요.</p>
-              {/* <button
+            <p className="text-2xl font-bold text-purple-600 mb-4">로그인 해주세요.</p>
+            {/* <button
                 onClick={handleKakaoLogin}
                 className="mt-6 px-4 py-2 bg-purple-600 text-white rounded-md shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">
                 카카오 로그인
               </button> */}
-              <button onClick={handleKakaoLogin}>
-                {' '}
-                {/* {nickname} */}
-                <Image src={'/images/kakaoLogin.png'} alt="kakao-login" width={200} height={200} />
-              </button>
-            </section>
-            <NavBottom />
-          </div>
+            <button onClick={handleKakaoLogin}>
+              {' '}
+              {/* {nickname} */}
+              <Image src={'/images/kakaoLogin.png'} alt="kakao-login" width={200} height={200} />
+            </button>
+          </section>
+          <NavBottom />
+          {/* </div> */}
         </>
       )}
     </div>
