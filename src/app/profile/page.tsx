9e9252721c4aa9dd77be279fb0890dfe6e7be38e
youngmatias <seyoit@gmail.com>
2024-06-20 04:51:37 +0200

    ui ready

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -134,7 +134,7 @@ export default function Page() {
         </>
       ) : (
         <>
-          <div className=" flex flex-col justify-center align-middle min-h-screen p-4">
+          <div className=" flex flex-col justify-center align-middle  p-4">
             <section
               className="wrap-section
             ">
