8742015599167a2c43fb0abc480bcfa2c6ede8d2
hsb2024 <subum6302@gmail.com>
2024-07-17 23:32:24 +0900

    style: 음악 모달 수정

--- src/components/todo/SearchModal.tsx
+++ src/components/todo/SearchModal.tsx
@@ -2,6 +2,7 @@ import { axios } from '@/services/instance';
 import { SetStateAction } from 'jotai';
 import React, { useState } from 'react';
 import ModalWrapper from '../ModalWrapper';
+import { MdSearch } from 'react-icons/md';
 
 interface Props {
   setIsModalOpen(boolean: boolean): void;
@@ -40,25 +41,34 @@ function SearchModal({ setIsModalOpen, setMusicTitle, setMusicUrl, postId }: Pro
     <ModalWrapper onClose={() => setIsModalOpen(false)}>
       <div className="w-full h-full flex justify-center items-center bg-black-950 bg-opacity-50 z-10">
         <div className="w-[15.625rem] h-[26.25rem] bg-white z-30" onClick={e => e.stopPropagation()}>
-          <form onSubmit={onSubmit}>
+          <form onSubmit={onSubmit} className="flex">
             <input
               type="text"
               value={value}
               onChange={getValue}
-              className="w-full py-2 pl-1 text-sm flex items-center border-b-[0.0313rem] border-borderGray"
-              placeholder="음악을 입력해주세요."></input>
+              className="w-full py-2 pl-1 flex items-center border-b-[0.0313rem] border-borderGray"
+              placeholder="음악을 입력해주세요."
+            />
+            <button className="justify-items-end">
+              <MdSearch size={32} color="black" />
+            </button>
           </form>
-          <div className="w-full h-[20rem] bg-white overflow-auto">
+          <div className="w-full h-[21.875rem] bg-white overflow-auto">
             <ul>
               {musicList &&
                 musicList.map((item, index) => (
-                  <li onClick={() => onMusic(item.title, item.singer, item.song_url)} key={index} className="py-1">
+                  <li
+                    onClick={() => onMusic(item.title, item.singer, item.song_url)}
+                    key={index}
+                    className="p-1 text-mm border-b-[0.0313rem] border-borderGray">
                     {item.singer} - {item.title}
                   </li>
                 ))}
             </ul>
           </div>
-          <div onClick={() => setIsModalOpen(false)} className="w-full border-t-[0.0313rem] border-borderGray">
+          <div
+            onClick={() => setIsModalOpen(false)}
+            className="w-full border-t-[0.0313rem] border-borderGray flex justify-end">
             <div className="w-fit h-fit p-1 text-sm cursor-pointer">닫기</div>
           </div>
         </div>
